4169cadedeffcf5a52a2cff2092223ba10183f3d
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-02-06 19:31:23 +0000

    adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
     <div className="App">
       <img src={reactLogo} className="logo"/>
       <h1>Hello World!</h1>
-      <span className="small">(from React)</span>
-      <h2><span className="from">by</span> Baz</h2>
+      <span className="fromt">(from React)</span>
+      <h2><span className="by">by</span> Baz</h2>
       <span className="emoji">😎</span>
       <button className="button" onClick={() => setCount((count) => count + 1)}>
         React.useState( )<br />Counter: <span className="count">{count}</span>
