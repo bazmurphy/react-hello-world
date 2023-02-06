775ec15599128c8c846e07b9bedcc769b34f9691
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-02-06 19:07:56 +0000

    changed from to by

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <img src={reactLogo} className="logo"/>
       <h1>Hello World!</h1>
       <span className="small">(from React)</span>
-      <h2><span className="from">from</span> Baz</h2>
+      <h2><span className="from">by</span> Baz</h2>
       <span className="emoji">😎</span>
       <button className="button" onClick={() => setCount((count) => count + 1)}>
         React.useState()<br />Counter: {count}
